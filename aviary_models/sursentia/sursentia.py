8383ecbdcaed60ebc208693932fc203a93a0f8af
mrsmrynk <marius.maryniak@w-hs.de>
2025-11-25 13:56:37 +0100

    refactor

--- aviary_models/sursentia/sursentia.py
+++ aviary_models/sursentia/sursentia.py
@@ -24,9 +24,12 @@ from typing import TYPE_CHECKING
 import numpy as np
 import pydantic
 import torch
-from aviary import AviaryUserError, RasterChannel, ChannelName
+from aviary import (
+    AviaryUserError,
+    ChannelName,
+    RasterChannel,
+)
 from aviary.tile import register_tiles_processor
-from huggingface_hub import hf_hub_download
 
 from aviary_models.sursentia.model import DINOUperNet
 from aviary_models.sursentia.sliding_window_inference import SlidingWindowInference
@@ -38,14 +41,28 @@ _PACKAGE = 'aviary-models'
 
 
 class Device(Enum):
+    """
+    Attributes:
+        CPU: CPU device
+        GPU: GPU device
+    """
     CPU = 'cpu'
     GPU = 'cuda'
 
     def to_torch(self) -> torch.Device:
+        """Converts the device to the torch device.
+
+        Returns:
+            Torch device
+        """
         return torch.device(self.value)
 
 
 class SursentiaVersion(Enum):
+    """
+    Attributes:
+        V1_0: Version 1.0
+    """
     V1_0 = '1.0'
 
 
@@ -82,16 +99,18 @@ class SursentiaConfig(pydantic.BaseModel):
             defaults to `ChannelName.G`
         b_channel_name: Channel name of the blue channel -
             defaults to `ChannelName.B`
-        landcover_channel_name: Channel name of the landcover channel -
+        landcover_channel_name: Channel name of the landcover channel (if None, the landcover head of the model
+            is not used) -
             defaults to 'sursentia_landcover'
-        solar_channel_name: Channel name of the solar channel -
+        solar_channel_name: Channel name of the solar panels channel (if None, the solar head of the model
+            is not used) -
             defaults to 'sursentia_solar'
-        batch_size: Batch size for sliding window inference -
+        batch_size: Batch size of the sliding window inference -
             defaults to 1
         version: Version of the model -
-            defaults to '1.0'  # todo
+            defaults to `SursentiaVersion.V1_0`
         device: Device to run the model on -
-            defaults to 'cpu'  # todo
+            defaults to `Device.CPU`
         cache_dir_path: Path to the cache directory of the model -
             defaults to 'cache'
         remove_channels: If True, the channels are removed -
@@ -100,8 +119,8 @@ class SursentiaConfig(pydantic.BaseModel):
     r_channel_name: ChannelName | str = ChannelName.R
     g_channel_name: ChannelName | str = ChannelName.G
     b_channel_name: ChannelName | str = ChannelName.B
-    landcover_channel_name: str = 'landcover'
-    solar_channel_name: str = 'solar'
+    landcover_channel_name: str = 'sursentia_landcover'
+    solar_channel_name: str = 'sursentia_solar'
     batch_size: int = 1
     version: SursentiaVersion = SursentiaVersion.V1_0
     device: Device = Device.CPU
@@ -111,15 +130,33 @@ class SursentiaConfig(pydantic.BaseModel):
 
 @register_tiles_processor(config_class=SursentiaConfig)
 class Sursentia:
-    """TODO
+    """Tiles processor that uses the Sursentia model to predict landcover and solar panels.
+
+    Model input channels:
+        - `ChannelName.R`: Red channel, raster channel, ground sampling distance of 0.1 to 1.0 meters,
+            standardized values with a mean of 0.392 and a standard deviation of 0.198
+        - `ChannelName.G`: Green channel, raster channel, ground sampling distance of 0.1 to 1.0 meters,
+            standardized values with a mean of 0.423 and a standard deviation of 0.173
+        - `ChannelName.B`: Blue channel, raster channel, ground sampling distance of 0.1 to 1.0 meters,
+            standardized values with a mean of 0.373 and a standard deviation of 0.157.
+
+    Model output channels:
+        - 'sursentia_landcover': Landcover channel, raster channel, ground sampling distance of the input channels,
+            the values are 0 (buildings), 1 (buildings with green roofs), 2 (impervious surfaces),
+            3 (non-impervious surfaces), and 4 (water bodies)
+        - 'sursentia_solar': Solar panels channel, raster channel, ground sampling distance of the input channels,
+            the values are 0 (background) and 1 (solar panels)
+
+    Additional dependencies:
+        Sursentia requires the `sursentia` dependency group and `torch`.
 
     Implements the `TilesProcessor` protocol.
     """
-    _HF_HUB_MODEL_PATHS = {
+    _HF_HUB_MODEL_PATHS = {  # noqa: RUF012
         SursentiaVersion.V1_0: {
             'landcover': 'models/v1_0/sursentia_landcover.ckpt',
             'solar': 'models/v1_0/sursentia_solar.ckpt',
-        }
+        },
     }
     _HF_HUB_REPO = 'geospaitial-lab/sursentia'
 
@@ -136,12 +173,30 @@ class Sursentia:
         cache_dir_path: Path = Path('cache'),
         remove_channels: bool = True,
     ) -> None:
+        """
+        Parameters:
+            r_channel_name: Channel name of the red channel
+            g_channel_name: Channel name of the green channel
+            b_channel_name: Channel name of the blue channel
+            landcover_channel_name: Channel name of the landcover channel (if None, the landcover head of the model
+                is not used)
+            solar_channel_name: Channel name of the solar panels channel (if None, the solar head of the model
+                is not used)
+            batch_size: Batch size of the sliding window inference
+            version: Version of the model
+            device: Device to run the model on
+            cache_dir_path: Path to cache directory of the model
+            remove_channels: If True, the channels are removed
+        """
         try:
             import torch  # noqa: PLC0415
+            import xformers  # noqa: F401, PLC0415
+            from huggingface_hub import hf_hub_download  # noqa: PLC0415
         except ImportError as error:
             message = (
-                'Missing dependency! '
-                'To use Sursentia, you need to install torch.'
+                'Missing dependencies! '
+                'To use Sursentia, you need to install the Sursentia dependency group '
+                '(pip install geospaitial-lab-aviary-models[sursentia]) and torch.'
             )
             raise ImportError(message) from error
 
@@ -153,90 +208,78 @@ class Sursentia:
         self._batch_size = batch_size
         self._version = version
         self._device = device.to_torch()
+        self._dtype = torch.float16
         self._cache_dir_path = cache_dir_path
         self._remove_channels = remove_channels
 
-        if self._device.type == "cpu":
-            os.environ["XFORMERS_DISABLED"] = "1"
+        if self._landcover_channel_name is None and self._solar_channel_name is None:
+            message = (
+                'Invalid landcover_channel_name / solar_channel_name! '
+                'At least one of the channel names must be specified.'
+            )
+            raise AviaryUserError(message)
+
+        if self._device.type == 'cpu':
+            os.environ['XFORMERS_DISABLED'] = '1'
             self._dtype = torch.float32
-        else:
-            self._dtype = torch.float16
 
         torch.hub.set_dir(self._cache_dir_path)
 
         if self._version not in self._HF_HUB_MODEL_PATHS:
             message = (
-                f'Unsupported version: {self._version}. '
-                'Supported versions: '
-                f'{list(self._HF_HUB_MODEL_PATHS.keys())}'
+                'Invalid version! '
+                'The version does not exist.'
             )
             raise AviaryUserError(message)
 
-        if self._landcover_channel_name is None and self._solar_channel_name is None:
-            message = (
-                'At least one of `landcover_channel_name` or `solar_channel_name` '
-                'must be specified.'
-            )
-            raise AviaryUserError(message)
+        hf_hub_model_paths = self._HF_HUB_MODEL_PATHS[self._version]
 
-        self._hf_hub_model_paths = self._HF_HUB_MODEL_PATHS[self._version]
+        landcover_ckpt = None
 
-        self._landcover_ckpt = None
         if self._landcover_channel_name is not None:
             landcover_ckpt_path = hf_hub_download(
                 repo_id=self._HF_HUB_REPO,
-                filename=self._hf_hub_model_paths['landcover'],
+                filename=hf_hub_model_paths['landcover'],
                 local_dir=self._cache_dir_path,
             )
-            self._landcover_ckpt = torch.load(landcover_ckpt_path, weights_only=False)
+            landcover_ckpt = torch.load(landcover_ckpt_path, weights_only=False)
 
-        self._solar_ckpt = None
+        solar_ckpt = None
+
         if self._solar_channel_name is not None:
             solar_ckpt_path = hf_hub_download(
                 repo_id=self._HF_HUB_REPO,
-                filename=self._hf_hub_model_paths['solar'],
+                filename=hf_hub_model_paths['solar'],
                 local_dir=self._cache_dir_path,
             )
-            self._solar_ckpt = torch.load(solar_ckpt_path, weights_only=False)
+            solar_ckpt = torch.load(solar_ckpt_path, weights_only=False)
 
-        if self._landcover_ckpt is not None and self._solar_ckpt is not None:
-            assert self._landcover_ckpt['hyperparameters']['arch'] == self._solar_ckpt['hyperparameters']['arch']
-            assert self._landcover_ckpt['hyperparameters']['backbone_name'] == self._solar_ckpt['hyperparameters']['backbone_name']
-            assert self._landcover_ckpt['hyperparameters']['intermediate_layers'] == self._solar_ckpt['hyperparameters']['intermediate_layers']
-            assert self._landcover_ckpt['hyperparameters']['pyramid_scales'] == self._solar_ckpt['hyperparameters']['pyramid_scales']
-            assert self._landcover_ckpt['hyperparameters']['num_channels_fpn'] == self._solar_ckpt['hyperparameters']['num_channels_fpn']
-            assert self._landcover_ckpt['hyperparameters']['ppm_scales'] == self._solar_ckpt['hyperparameters']['ppm_scales']
-            assert self._landcover_ckpt['hyperparameters']['patch_size'] == self._solar_ckpt['hyperparameters']['patch_size']
+        ckpt = landcover_ckpt if landcover_ckpt is not None else solar_ckpt
 
-            backbone_name = self._landcover_ckpt['hyperparameters']["backbone_name"]
-            patch_size = self._landcover_ckpt['hyperparameters']['patch_size']
-        elif self._landcover_ckpt is not None:
-            backbone_name = self._landcover_ckpt['hyperparameters']["backbone_name"]
-            patch_size = self._landcover_ckpt['hyperparameters']['patch_size']
-        elif self._solar_ckpt is not None:
-            backbone_name = self._solar_ckpt['hyperparameters']["backbone_name"]
-            patch_size = self._solar_ckpt['hyperparameters']['patch_size']
-        else:
-            raise AviaryUserError("No checkpoint found")
+        if ckpt is None:
+            message = 'Invalid checkpoint!'
+            raise AviaryUserError(message)
+
+        backbone_name = ckpt['hyperparameters']['backbone_name']
+        patch_size = ckpt['hyperparameters']['patch_size']
 
         self._model = DINOUperNet(
             backbone_name=backbone_name,
-            landcover_ckpt=self._landcover_ckpt,
-            solar_ckpt=self._solar_ckpt,
+            landcover_ckpt=landcover_ckpt,
+            solar_ckpt=solar_ckpt,
             landcover_out_name=self._landcover_channel_name,
             solar_out_name=self._solar_channel_name,
         )
-        self._model.requires_grad_(False)
+        self._model.requires_grad_(requires_grad=False)
         self._model.eval()
         self._model.to(self._device)
         self._model.to(self._dtype)
-        # self._model.compile()
 
         self._sliding_window_inference = SlidingWindowInference(
             window_size=patch_size,
             batch_size=self._batch_size,
             model_receptive_field=None,
-            overlap=0.5,
+            overlap=.5,
             downweight_edges=True,
         )
 
@@ -273,23 +316,24 @@ class Sursentia:
             self._g_channel_name,
             self._b_channel_name,
         ]
+
         inputs = tiles.to_composite_raster(channel_names=channel_names)
         inputs = torch.from_numpy(inputs).to(self._dtype).to(self._device).permute(0, 3, 1, 2)
-        inputs = {"tensor": inputs}
+        inputs = {'tensor': inputs}
 
-        logits_dict = self._sliding_window_inference(self._model, inputs)
+        logits_dict = self._sliding_window_inference(
+            model=self._model,
+            batch=inputs,
+        )
 
-        for key, value in logits_dict.items():
-            value = value.cpu().numpy()
-            value = np.argmax(value, axis=1)
-            value = value.astype(np.uint8)
+        for channel_name, logits in logits_dict.items():
+            preds = np.argmax(logits.cpu().numpy(), axis=1).astype(np.uint8)
 
-            data = list(value)
-            out_channel_name = key
+            data = list(preds)
             buffer_size = tiles[channel_names[0]].buffer_size
             preds_channel = RasterChannel(
                 data=data,
-                name=out_channel_name,
+                name=channel_name,
                 buffer_size=buffer_size,
                 copy=False,
             )
