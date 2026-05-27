7443ed77208373cfaed6b161821ff75178ef07a7
mrsmrynk <marius.maryniak@w-hs.de>
2026-05-27 12:46:57 +0200

    fix linting

--- aviary_models/sursentia/sursentia.py
+++ aviary_models/sursentia/sursentia.py
@@ -16,10 +16,12 @@
 
 from __future__ import annotations
 
-import uuid
 from enum import Enum
 from pathlib import Path
 from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import uuid
 
 import numpy as np
 import pydantic
